4c90a8cc2305b0089cfe89fdb94591e1a9b3a34b
robert <robert.constantin@optivamedia.com>
2023-02-25 10:49:11 +0100

    feat (appState): add app state

---
+++ app/src/main/java/com/example/composeexpert/coreUi/main/JetAppState.kt
@@ -0,0 +1,103 @@
+package com.example.composeexpert.coreUi.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import com.example.composeexpert.coreUi.navigation.TopLevelDestination
+import com.example.composeexpert.feature.addX.addNavigationRoute
+import com.example.composeexpert.feature.addX.navigateToAddScreen
+import com.example.composeexpert.feature.favorites.favoritesNavigationRoute
+import com.example.composeexpert.feature.favorites.mainFeedNavigationRoute
+import com.example.composeexpert.feature.favorites.navigateToFavoriteScreen
+import com.example.composeexpert.feature.favorites.navigateToMainFeedScreen
+import com.example.composeexpert.feature.settings.navigateToSettingsScreen
+import com.example.composeexpert.feature.settings.settingsNavigationRoute
+import kotlinx.coroutines.CoroutineScope
+
+/**
+ * Function that establish the entry point of app state.
+ *
+ * @param navController Needed to carry out navigation at appState
+ */
+@Composable
+fun rememberJetAppState(
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    navController: NavHostController = rememberNavController(),
+): JetAppState = remember(coroutineScope, navController) {
+    JetAppState(navController, coroutineScope)
+}
+
+
+/**
+ * Class that manages general appState (currentDestinations, topLevelDestinations,
+ * general on screen app composables like dialogs, network listening, navigation management etc...)
+ */
+@Stable
+class JetAppState(
+    private val navController: NavHostController,
+    val coroutineScope: CoroutineScope
+) {
+    //gives you the visible composable that the user is currently seeing.
+    private val currentDestination: NavDestination?
+        @Composable get() = navController.currentBackStackEntryAsState().value?.destination
+
+    val currentTopLevelDestination: TopLevelDestination?
+        @Composable get() = when (currentDestination?.route) {
+            mainFeedNavigationRoute -> TopLevelDestination.MAIN_FEED
+            favoritesNavigationRoute -> TopLevelDestination.FAVORITES_SCREEN
+            settingsNavigationRoute -> TopLevelDestination.SETTINGS_SCREEN
+            addNavigationRoute -> TopLevelDestination.ADD_SCREEN
+            else -> null
+        }
+
+    //this could be done by using enum, so that here in only just one line we have our list.
+//    val topLevelDestinations: List<TopLevelDestination2> = listOf(
+//        TopLevelDestination2.MainFeedScreen,
+//        TopLevelDestination2.FavoritesScreen,
+//        TopLevelDestination2.SettingScreen,
+//        TopLevelDestination2.AddScreen
+//    )
+
+    private val topLevelDestinations: List<TopLevelDestination> = TopLevelDestination.values().asList()
+
+
+    fun navigateToTopLevelDestination(topLevelDestination: TopLevelDestination) {
+        navOptions {
+            // Pop up to the start destination of the graph to
+            // avoid building up a large stack of destinations
+            // on the back stack as users select items
+            popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+            }
+            // Avoid multiple copies of the same destination when
+            // reselecting the same item
+            launchSingleTop = true
+            // Restore state when reselecting a previously selected item
+            restoreState = true
+        }.also { navOptions ->
+            when (topLevelDestination) {
+                TopLevelDestination.MAIN_FEED ->
+                    navController.navigateToMainFeedScreen(navOptions)
+                TopLevelDestination.FAVORITES_SCREEN ->
+                    navController.navigateToFavoriteScreen(navOptions)
+                TopLevelDestination.SETTINGS_SCREEN ->
+                    navController.navigateToSettingsScreen(navOptions)
+                TopLevelDestination.ADD_SCREEN ->
+                    navController.navigateToAddScreen(navOptions)
+            }
+        }
+    }
+
+    val showBottomNavigation: Boolean
+        @Composable get () = topLevelDestinations.any {
+            currentDestination?.route.orEmpty().contains(it.route)
+        }
+
+}
